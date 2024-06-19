2bcaae2ce0af6ce88b0fdccd25ce30cce6ae3eec
Daniel Stamer <dan@hello-world.sh>
2024-06-19 11:27:25 -0300

    Add metadata tests

--- metadata_test.go
+++ metadata_test.go
@@ -18,6 +18,181 @@ import (
 	"testing"
 )
 
+func Test_port(t *testing.T) {
+	envVarKey := "PORT"
+	envVarVal := "8081"
+
+	err := os.Setenv(envVarKey, "")
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err := port()
+	if err == nil {
+		t.Fatalf(`port() = %s, %v, want 0, error`, result, err)
+	}
+
+	err = os.Setenv(envVarKey, envVarVal)
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err = port()
+	if result != envVarVal || err != nil {
+		t.Fatalf(`port() = %s, %v, want %s, error`, result, err, envVarVal)
+	}
+}
+
+func Test_kNativeService(t *testing.T) {
+	envVarKey := "K_SERVICE"
+	envVarVal := "service-001"
+
+	err := os.Setenv(envVarKey, "")
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err := kNativeService()
+	if err == nil {
+		t.Fatalf(`kNativeService() = %s, %v, want 0, error`, result, err)
+	}
+
+	err = os.Setenv(envVarKey, envVarVal)
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err = kNativeService()
+	if result != envVarVal || err != nil {
+		t.Fatalf(`kNativeService() = %s, %v, want %s, error`, result, err, envVarVal)
+	}
+}
+
+func Test_kNativeRevision(t *testing.T) {
+	envVarKey := "K_REVISION"
+	envVarVal := "revision-001"
+
+	err := os.Setenv(envVarKey, "")
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err := kNativeRevision()
+	if err == nil {
+		t.Fatalf(`kNativeRevision() = %s, %v, want 0, error`, result, err)
+	}
+
+	err = os.Setenv(envVarKey, envVarVal)
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err = kNativeRevision()
+	if result != envVarVal || err != nil {
+		t.Fatalf(`kNativeRevision() = %s, %v, want %s, error`, result, err, envVarVal)
+	}
+}
+
+func Test_jobName(t *testing.T) {
+	envVarKey := "CLOUD_RUN_JOB"
+	envVarVal := "job-001"
+
+	err := os.Setenv(envVarKey, "")
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err := jobName()
+	if err == nil {
+		t.Fatalf(`jobName() = %s, %v, want 0, error`, result, err)
+	}
+
+	err = os.Setenv(envVarKey, envVarVal)
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err = jobName()
+	if result != envVarVal || err != nil {
+		t.Fatalf(`jobName() = %s, %v, want %s, error`, result, err, envVarVal)
+	}
+}
+
+func Test_jobExecution(t *testing.T) {
+	envVarKey := "CLOUD_RUN_EXECUTION"
+	envVarVal := "job-execution-001"
+
+	err := os.Setenv(envVarKey, "")
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err := jobExecution()
+	if err == nil {
+		t.Fatalf(`jobExecution() = %s, %v, want 0, error`, result, err)
+	}
+
+	err = os.Setenv(envVarKey, envVarVal)
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err = jobExecution()
+	if result != envVarVal || err != nil {
+		t.Fatalf(`jobExecution() = %s, %v, want %s, error`, result, err, envVarVal)
+	}
+}
+
+func Test_jobTaskIndex(t *testing.T) {
+	envVarKey := "CLOUD_RUN_TASK_INDEX"
+	envVarVal := 12
+
+	err := os.Setenv(envVarKey, "wrong value")
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err := jobTaskIndex()
+	if err == nil {
+		t.Fatalf(`jobTaskIndex() = %d, %v, want 0, error`, result, err)
+	}
+
+	err = os.Setenv(envVarKey, fmt.Sprintf("%d", envVarVal))
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err = jobTaskIndex()
+	if result != envVarVal || err != nil {
+		t.Fatalf(`jobTaskIndex() = %d, %v, want %d, error`, result, err, envVarVal)
+	}
+}
+
+func Test_jobTaskAttempt(t *testing.T) {
+	envVarKey := "CLOUD_RUN_TASK_ATTEMPT"
+	envVarVal := 14
+
+	err := os.Setenv(envVarKey, "wrong value")
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err := jobTaskAttempt()
+	if err == nil {
+		t.Fatalf(`jobTaskAttempt() = %d, %v, want 0, error`, result, err)
+	}
+
+	err = os.Setenv(envVarKey, fmt.Sprintf("%d", envVarVal))
+	if err != nil {
+		t.Fatalf("unable to test: %v", err)
+	}
+
+	result, err = jobTaskAttempt()
+	if result != envVarVal || err != nil {
+		t.Fatalf(`jobTaskAttempt() = %d, %v, want %d, error`, result, err, envVarVal)
+	}
+}
+
 func Test_jobTaskCount(t *testing.T) {
 	envVarKey := "CLOUD_RUN_TASK_COUNT"
 	envVarVal := 16
